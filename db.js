fe48aeb5f321a326cb7c7f9b78d9e7d36f8fc859
Pavanaprana Dasa <pavamana1123@gmail.com>
2024-01-27 13:30:52 +0530

    otp fixed

--- db.js
+++ db.js
@@ -3,15 +3,63 @@ class DB {
         this.db=db
     }
 
-    execQuery(q){
+    exec(q){
         return new Promise((resolve, reject) => {
             this.db.query(q, (e, result) => {
                 if (e) {
                     return reject(e)
-                };  
+                }  
                 resolve(result)
-            });
-        });
+            })
+        })
+    }
+
+    storeOTP(id, otp, target, expiry){
+        expiry = expiry || 300
+        return new Promise((resolve, reject) => {
+            this.db.query(
+                `insert into otp (id, target, otp, expiry) values("${id}","${target}","${otp}", now()+ interval ${expiry} second)
+                on duplicate key update otp="${otp}", expiry=now()+ interval ${expiry} second, target="${target}"`,
+
+                (error, result) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        if(result.affectedRows>0){
+                            resolve()
+                        }else{
+                            reject("No rows affected")
+                        }
+                    }
+                }
+            )
+        })
+    }
+
+    verifyOTP(id, otp){
+
+        return new Promise((resolve, reject) => {
+            this.db.query(
+                `select otp from otp where id='${id}'`,
+
+                (error, result) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        if(result.length==0){
+                            reject({status: 404, error: "OTP not found"})
+                        }else{
+                            if(result[0].otp==otp){
+                                resolve()
+                            }else{
+                                reject({status: 403, error: "OTP mismatch"})
+                            }
+                        }
+                    }
+                }
+            )
+        })
+
     }
 }
 
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 async function main() {
   const express = require('express')
+  const axios = require('axios')
+  const otpGenerator = require('otp-generator')
+
   const app = express()
   app.use(express.json()) 
   const port = 4005
 
-  const API  = require("./api.js")
   var cred = require("./cred.js")
   const DB = require("./db.js")
   const Mail = require("./mail.js")
@@ -17,23 +19,81 @@ async function main() {
   var mail = new Mail(cred.mail)
   await mail.verify()
 
-  const api = new API(db, mail)
-  app.post('/data', api.call.bind(api))
+  const wameURL = 'https://wame.iskconmysore.org'
+  const adminMail = 'admin@otp.iskconmysore.org'
+  const template = 'ekakalika_guhyapadam'
+
+  const sendOTP = (target, title, otp) => {
+    if(target.includes('@')){
+      return mail.send({
+        from: adminMail,
+        to: target,
+        subject: title || 'OTP from ISKCON Mysore',
+        text: `Hare Krishna!\nYour OTP is ${otp}`
+      })
+    }else{
+      return axios.post(wameURL, {
+        phone: target,
+        template,
+        headers: [title],
+        values: [ "OTP", otp ]
+      }, {
+        headers: {
+          "api-key": cred.wame.apiKey
+        }
+      })
+    }
+  }
+
+  app.post('/data', (req, res)=>{
+    var endpoint = req.get("endpoint")
+    var reqAPIKey = req.get("api-key")
+
+    if(cred.apiKey == reqAPIKey){
+      const { title, id, otp, target, len } = req.body
+
+      if(endpoint=="/send"){
+        const otpGen = otpGenerator.generate(len||6, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
+        sendOTP(target, title, otpGen)
+        .then(r=>{
+          db.storeOTP(id, otpGen, target)
+          .then(r=>{
+            res.status(200).send(r)
+          }).catch((err)=>{
+            res.status(500).send(err)
+          })
+        }).catch(err=>{
+          console.log(err)
+          res.status(err.response.status).send(err.response.data.message)
+        })
+      }else if(endpoint=="/verify"){
+        db.verifyOTP(id, otp)
+        .then(r=>{
+          res.status(200).send(r)
+        }).catch(({status, error})=>{
+          res.status(status).send(error)
+        })
+      }else{
+        res.status(400).send()
+      }
+    }else{
+      res.status(401).send()
+    }
+  })
 
   app.listen(port, () => {
     console.log(`OTP app listening at http://localhost:${port}`)
   })
 
   setInterval(() => {
-    var query = `delete FROM iskconmy_otp.otp where expiry<=now();`
-    db.execQuery(query)
+    db.exec(`delete FROM iskconmy_otp.otp where expiry<=now();`)
     .then((result) => {
         if(result.affectedRows>0){
           console.log(`${result.affectedRows} OTP expired.`)
         }
     })
     
-  }, 60000)
+  }, 60*1000)
 }
 
 main().catch((e) => {
